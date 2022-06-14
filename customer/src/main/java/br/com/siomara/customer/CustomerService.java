40fcb4a1585a99db191e5e2bd1faf610661eb48b
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-14 00:46:36 -0300

    Add CustomerService class

---
+++ customer/src/main/java/br/com/siomara/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package br.com.siomara.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        //todo: check if email is valid
+        //todo: check if email not taken
+        //todo: store customer in db
+
+    }
+}
