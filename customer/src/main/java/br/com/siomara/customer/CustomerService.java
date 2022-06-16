b5de69745537a0e390334b986e93f2477be6cf84
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-16 16:31:53 -0300

    Create 'customer' objects and table into database

--- customer/src/main/java/br/com/siomara/customer/CustomerService.java
+++ customer/src/main/java/br/com/siomara/customer/CustomerService.java
@@ -3,7 +3,7 @@ package br.com.siomara.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,7 +12,9 @@ public record CustomerService() {
                 .build();
         //todo: check if email is valid
         //todo: check if email not taken
-        //todo: store customer in db
+
+        // Store customer into database
+        customerRepository.save(customer);
 
     }
 }
