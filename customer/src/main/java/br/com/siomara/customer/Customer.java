3fa0ff99efd19422aba2b64890eaaace6979d25c
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-16 19:23:14 -0300

    Individualized persistence imports

--- customer/src/main/java/br/com/siomara/customer/Customer.java
+++ customer/src/main/java/br/com/siomara/customer/Customer.java
@@ -5,7 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Data
 @Builder
