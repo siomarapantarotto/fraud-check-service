cf9fcde835e902931001ef09fb33d92b594a6d01
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-17 01:06:14 -0300

    Create module fraud

---
+++ fraud/src/main/java/br/com/siomara/fraud/FraudApplication.java
@@ -0,0 +1,11 @@
+package br.com.siomara.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
