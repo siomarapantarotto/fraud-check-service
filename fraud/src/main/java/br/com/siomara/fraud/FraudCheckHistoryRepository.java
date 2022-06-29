de7a2a23cfd153c32850dffbae9f2428a3ad8c5a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-28 21:45:22 -0300

    Create FraudCheckHistoryRepository

---
+++ fraud/src/main/java/br/com/siomara/fraud/FraudCheckHistoryRepository.java
@@ -0,0 +1,7 @@
+package br.com.siomara.fraud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository
+        extends JpaRepository<FraudCheckHistory, Integer> {
+}
