11b1dc4e987b3a6ecfe8e8d87ba96d2687183777
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-30 20:55:36 -0300

    Create fraud service as regular class instead of record

---
+++ fraud/src/main/java/br/com/siomara/fraud/FraudCheckService.java
@@ -0,0 +1,23 @@
+package br.com.siomara.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+    public boolean isFraudulentCustomer (Integer customerId) {
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        return false; // for now just return false
+    }
+}
