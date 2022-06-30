87eaff16850ff4c4fcfe072e6ac633b959b71a2d
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-30 20:54:40 -0300

    Create fraud controller as regular class instead of record

---
+++ fraud/src/main/java/br/com/siomara/fraud/FraudCheckController.java
@@ -0,0 +1,20 @@
+package br.com.siomara.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/fraud-check")
+@AllArgsConstructor
+public class FraudCheckController {
+
+    private final FraudCheckService fraudCheckService;
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(
+            @PathVariable("customerId") Integer customerId) {
+        boolean isFraudulentCustomer = fraudCheckService.
+                isFraudulentCustomer(customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}
