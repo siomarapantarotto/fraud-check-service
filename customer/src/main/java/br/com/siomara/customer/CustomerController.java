c2b515c4f3e23c2903a97c2542d3e3412d2a1b6a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-06-30 20:58:46 -0300

    Change customer controller/service to regular class instead of record

--- customer/src/main/java/br/com/siomara/customer/CustomerController.java
+++ customer/src/main/java/br/com/siomara/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package br.com.siomara.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController(CustomerService customerService) {
+@AllArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
 
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("New customer registration {}", customerRegistrationRequest);
         customerService.registerCustomer(customerRegistrationRequest);
-
     }
-
 }
--- customer/src/main/java/br/com/siomara/customer/CustomerService.java
+++ customer/src/main/java/br/com/siomara/customer/CustomerService.java
@@ -1,9 +1,12 @@
 package br.com.siomara.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
